d14a6dbf58d364f7582dd446de05961cc2cd5ba9
olleholt <olivialeighholt97@gmail.com>
2023-08-18 14:27:15 -0700

    Initial commit

---
+++ R/beep_boop_function.R
@@ -0,0 +1,11 @@
+
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+beep_boop <- function(){
+  paste("beep boop I am a function")
+}
---
+++ R/burritos_cost.R
@@ -0,0 +1,13 @@
+#' Title calculte Total Burrito Cost
+#'
+#' @param price price per burrito in dollars
+#' @param count number of burritos in order
+#'
+#' @return
+#' @export
+#'
+#' @examples
+burrito_cost <- function(price, count) {
+  price * count
+}
+
